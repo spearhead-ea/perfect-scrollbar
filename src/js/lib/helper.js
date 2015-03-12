cf868039280de30dd987cf4d3506d77529e942d0
Choi Damdam <damchoi@ea.com>
2015-03-12 12:02:25 +0900

    Revert "parseInt() returns NaN when the first argument is empty string. ex) parseInt('', 10) -> NaN"
    This reverts commit 4524c8c10090d9773ea7d058903adefe40c42472.

--- src/js/lib/helper.js
+++ src/js/lib/helper.js
@@ -8,7 +8,7 @@ var cls = require('./class')
 
 exports.toInt = function (x) {
   if (typeof x === 'string') {
-    return parseInt(x, 10) || 0;
+    return parseInt(x, 10);
   } else {
     return ~~x;
   }
